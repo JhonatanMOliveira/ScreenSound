956d874a92c553acc0dde6863eee105b6a538f8b
jhonnyseth <jhonatan.mallet@gmail.com>
2024-09-20 15:04:52 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,24 @@
+﻿Banda queen = new Banda("Queen");
+
+Album albumDoQueen = new Album("A Night at the Opera");
+
+Musica musica1 = new Musica(queen, "Love of my Life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody") 
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoQueen.ExibirMusicasDoAlbum();
+queen.ExibirDiscografia();
